3c94d778752becc0c8be0f335ccd5221b5669253
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-10-14 13:27:41 -0700

    Add store to main index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
 
 import Routes from './routes/';
+import store from './redux/store';
 import './index.css';
 import muiTheme from './config/themes';
 import registerServiceWorker from './registerServiceWorker';
 
 const MyTransactions = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
-    <Router>
-        <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router>
+          <Routes />
+      </Router>
+    </Provider>
   </MuiThemeProvider>
 )
 
